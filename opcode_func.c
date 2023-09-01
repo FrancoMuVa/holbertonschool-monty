b3695257981d843eb7a268ec0205a7bc12674df7
FrancoMuVa <muvafranco@gmail.com>
2023-09-01 13:48:35 +0000

    fix: style

--- opcode_func.c
+++ opcode_func.c
@@ -113,7 +113,7 @@ void opcode_pint(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", temp->n);
